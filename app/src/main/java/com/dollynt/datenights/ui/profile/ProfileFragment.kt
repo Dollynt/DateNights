c111d3ec17f50d34678d62f78da56ecb7277267d
Dollynt <luiz.guilherme1319@gmail.com>
2024-08-06 13:14:38 -0300

    feat(login): readd logout

--- app/src/main/java/com/dollynt/datenights/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/dollynt/datenights/ui/profile/ProfileFragment.kt
@@ -1,11 +1,15 @@
 package com.dollynt.datenights.ui.profile
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.dollynt.datenights.databinding.FragmentProfileBinding
+import com.dollynt.datenights.ui.login.LoginActivity
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class ProfileFragment : Fragment() {
 
@@ -23,6 +27,10 @@ class ProfileFragment : Fragment() {
         }
         binding.buttonLogout.setOnClickListener {
             // Ação para logout
+            Firebase.auth.signOut()
+            val intent = Intent(activity, LoginActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
         }
 
         return binding.root
